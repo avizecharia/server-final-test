2a9ac5723c916ba0251bd4c1c9bf72dd6c4569da
aviel <aviel469@gmail.com>
2024-12-18 15:34:07 +0200

    add third model

---
+++ src/models/third.model.ts
@@ -0,0 +1,21 @@
+import { model, Schema } from "mongoose"
+import {IThird} from "../types/interfaces"
+
+
+export const thirdSchema = new Schema<IThird>({
+    year: {
+        type: Number,
+        required: true
+    },
+    month: {
+        type: Number,
+        default: 0
+    },
+    numEvent: {
+        type: Number,
+        default: 0
+    }
+})
+
+
+export const thirdModel = model('third', thirdSchema)
