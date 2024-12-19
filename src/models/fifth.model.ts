9a984889035be57244366da44c0495b894b7abb0
aviel <aviel469@gmail.com>
2024-12-19 10:00:15 +0200

    ctrl s

--- src/models/fifth.model.ts
+++ src/models/fifth.model.ts
@@ -1,18 +1,19 @@
-import { model, Schema } from "mongoose"
-import { IFifth } from "../types/interfaces"
-
-
+import { model, Schema } from "mongoose";
+import { IFifth } from "../types/interfaces";
 
 export const fifthSchema = new Schema<IFifth>({
-    organizationName: {
-        type: String,
-        required: true
-    },
-    numEnent: {
-        type: Number,
-        default:0
-    } 
-})
+  organizationName: {
+    type: String,
+    required: true,
+  },
+  numEvent: {
+    type: Number,
+    default: 0,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+});
 
-
-export const fifthModel = model('fifth', fifthSchema)
+export const fifthModel = model("fifth", fifthSchema);
