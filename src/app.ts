82f73372f6dc4c27a46d59caa0fd8b4db672e583
aviel <aviel469@gmail.com>
2024-12-19 16:37:49 +0200

    add socket to app

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,31 @@ import "dotenv/config";
 import { connentToMongo } from "./config/db";
 import analysisRoute from "./routes/analysis.route";
 import relationshipsRoute from "./routes/relationships.route";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import { handelConnection } from "./soket/io";
 
 connentToMongo();
+
 const app = express();
+
+export const server = http.createServer(app);
+
 const PORT = process.env.PORT || 3000;
 
+export const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: "*",
+  },
+});
+
+app.use(cors());
 app.use("/api/analysis", analysisRoute);
 app.use("/api/relationships", relationshipsRoute);
+
+io.on("connection", handelConnection);
 
 app.listen(PORT, () => {
   console.log(`server is runnig on port http://localhost:${PORT}`);
