f216eada2e2639ae8a90ac29cfe835876c3a1c42
aviel <aviel469@gmail.com>
2024-12-19 15:51:27 +0200

    wire end point analysis and relationship

--- src/routes/analysis.route.ts
+++ src/routes/analysis.route.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import {
+  getFirstData,
+  getSecData,
+  getThirdData,
+} from "../controllers/analysis.controller";
 
 const router = Router();
 
-router.get("/deadliest-attack-types", () => {});
-router.get("/highest-casualty-regions", () => {});
-router.get("/incident-trends", () => {});
+router.get("/deadliest-attack-types", getFirstData);
+router.get("/highest-casualty-regions", getSecData);
+router.get("/incident-trends", getThirdData);
 
 export default router;
--- src/routes/relationships.route.ts
+++ src/routes/relationships.route.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import {
+  getFifthDataAll,
+  getForthDataAll,
+  getSixthData,
+} from "../controllers/relationships.controller";
 
 const router = Router();
 
-router.get("/top-groups", () => {});
-router.get("//groups-by-year", () => {});
-router.get("/deadliest-regions", () => {});
+router.get("/top-groups", getForthDataAll);
+router.get("//groups-by-year", getFifthDataAll);
+router.get("/deadliest-regions", getSixthData);
 
 export default router;
