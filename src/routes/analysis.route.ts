5c07fc1a0e91f3aa6b0fa86fb3c32138b9f07afe
aviel <aviel469@gmail.com>
2024-12-23 16:12:33 +0200

    add end point for testing

--- src/routes/analysis.route.ts
+++ src/routes/analysis.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   getFirstData,
   getSecData,
+  getsecRData,
   getThirdData,
 } from "../controllers/analysis.controller";
 
@@ -10,5 +11,5 @@ const router = Router();
 router.get("/deadliest-attack-types", getFirstData);
 router.get("/highest-casualty-regions", getSecData);
 router.get("/incident-trends", getThirdData);
-
+router.get('/f',getsecRData)
 export default router;
